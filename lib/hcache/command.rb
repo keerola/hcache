978ec49d6d4f1617d289d53e832b9341484173c1
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2010-04-15 22:21:14 +0300

    hcache: Add the DependencyCommand class

--- lib/hcache/command.rb
+++ lib/hcache/command.rb
@@ -6,7 +6,7 @@ module Hcache
       @args = args.clone
       @cache_dir = cache_dir
       @default_includes = default_includes
-      @command = get_command 
+      @command = get_command
     end
 
     def get_command
@@ -21,6 +21,33 @@ module Hcache
       @command
     end
 
+  end
+
+  class DependencyCommand < CommandBase
+  
+    def initialize(args, cache_dir, default_includes)
+      super
+    end
+  
+    def get_command
+      command = super
+      old_includes = ""
+      while not @args.empty? do
+        arg = @args.shift
+        if arg.match(/^\-I/)
+          include_dir = arg[2, arg.length]
+          command += " -I#{@cache_dir}#{include_dir}"
+          old_includes += " -I#{include_dir}"
+        elsif arg == "-o"
+          @args.shift
+        else
+          command += " #{arg}"    
+        end
+      end
+      command += " -M -MP"
+      command += old_includes
+    end
+  
   end
 
   class Command < CommandBase
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -15,3 +15,16 @@ describe CommandBase do
 
 end
 
+describe DependencyCommand do
+
+  ARGS = ['foo', '-Ibar', '-o', 'baz.o', 'baz.c']
+  HCACHE_DIR = 'qux/'
+  GCC_DEFAULT_INCLUDES = []
+  
+  it "removes -o option and adds -M and -MP options" do
+    command = DependencyCommand.new(ARGS, HCACHE_DIR, GCC_DEFAULT_INCLUDES)
+    command.to_s.should == "foo -Iqux/bar baz.c -M -MP -Ibar"
+  end
+
+end
+
